3cca5707313cfa5c72aa66857ae602355c24225a
khado1 <kha-nguyen@outlook.com>
2022-04-05 20:48:58 +0200

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import debounce from "lodash.debounce";
 import { useState, useMemo } from "react";
 import "./App.scss";
 import json from "./data/nameSearchPop300.json";
 import { getNameCount } from "./util/getNameCount";
-import { Title, Input, Button } from "@statisticsnorway/ssb-component-library";
+import {
+  Title,
+  Input,
+  Button,
+  Divider,
+} from "@statisticsnorway/ssb-component-library";
 
 window.data = json.response.docs;
 function App() {
   const [search, setSearch] = useState("");
   const [counts, setCounts] = useState(null);
+  const [key, setKey] = useState(0);
 
   function results() {
     if (search) {
@@ -17,27 +22,60 @@ function App() {
     }
   }
 
+  const clearInput = () => {
+    setSearch("");
+    setCounts(null);
+    setKey((k) => k + 1);
+  };
+
   //console.log(getNameCount(json.response.docs, search));
   const onSearch = (value) => {
     setSearch(value);
   };
   return (
     <div className="main-container">
-      <div className="information-container">
+      <div className="search-container">
         <Title size="2">Navnesøk</Title>
         <p>Hvor mange heter det samme som deg?</p>
 
-        <Input placeholder="Search for name..." handleChange={onSearch} />
+        <Input
+          placeholder="Skriv inn navn her..."
+          handleChange={onSearch}
+          key={key}
+        />
         <br />
-        <Button primary onClick={results}>
+        <Button primary onClick={results} disabled={!search}>
           Se resultat
         </Button>
-
+      </div>
+      <div>
         {counts && (
-          <div>
+          <div className="results-information">
+            <Title size="2">Resultat</Title>
+            <Divider dark />
+            <br></br>
             {counts.map(([name, values]) => (
               <Card name={name} counts={values} />
             ))}
+            <div className="button-center">
+              <Button onClick={clearInput}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18"></line>
+                  <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+                Lukk
+              </Button>
+            </div>
           </div>
         )}
       </div>
@@ -52,34 +90,43 @@ const Card = ({ name, counts }) => {
   const sumAllThree = sumForname + counts.family;
   return (
     <div>
-      {sumAllThree < 4 ? (
-        <h4>
-          Det er færre enn fire eller ingen som heter{" "}
-          <span className="name">{name}</span>
-        </h4>
-      ) : (
-        <>
-          <h3></h3>
-          {sumForname > 0 && sumForname !== counts.onlygiven && (
-            <h4>
-              Det er {sumForname} som har <span className="name">{name}</span>{" "}
-              som sitt første fornavn
-            </h4>
-          )}
-          {counts.onlygiven > 0 && (
-            <h4>
-              Det er {counts.onlygiven} som har{" "}
-              <span className="name">{name}</span> som sitt eneste fornavnavn
-            </h4>
-          )}
-          {counts.family > 0 && (
-            <h4>
-              Det er {counts.family} som har{" "}
-              <span className="name">{name}</span> som sitt etternavn
-            </h4>
-          )}
-        </>
-      )}
+      <ul className="ba">
+        {sumAllThree < 4 ? (
+          <li>
+            <span>
+              Det er færre enn fire eller ingen som heter{" "}
+              <span className="name">{name}</span>
+            </span>
+          </li>
+        ) : (
+          <>
+            {sumForname > 0 && sumForname !== counts.onlygiven && (
+              <li>
+                <span>
+                  Det er {sumForname} som har{" "}
+                  <span className="name">{name}</span> som sitt første fornavn
+                </span>
+              </li>
+            )}
+            {counts.onlygiven > 0 && (
+              <li>
+                <span>
+                  Det er {counts.onlygiven} som har{" "}
+                  <span className="name">{name}</span> som sitt eneste fornavn
+                </span>
+              </li>
+            )}
+            {counts.family > 0 && (
+              <li>
+                <span>
+                  Det er {counts.family} som har{" "}
+                  <span className="name">{name}</span> som sitt etternavn
+                </span>
+              </li>
+            )}
+          </>
+        )}
+      </ul>
     </div>
   );
 };
